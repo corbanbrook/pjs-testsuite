e30cba15b36044f374deae1c72217bf4c094a710
Corban Brook <corbanbrook@gmail.com>
2010-04-01 23:56:01 -0400

    Fixed migration for osx 10.6 detection

--- db/migrate/20100329171659_create_platforms.rb
+++ db/migrate/20100329171659_create_platforms.rb
@@ -7,7 +7,7 @@ class CreatePlatforms < ActiveRecord::Migration
     end
 
     Platform.create :name => "Mac OS X", :version => "10.5", :regexp => "Mac OS X 10[_.]5"
-    Platform.create :name => "Mac OS X", :version => "10.6", :regexp => "MacOS X 10[_.]6"  
+    Platform.create :name => "Mac OS X", :version => "10.6", :regexp => "Mac OS X 10[_.]6"  
     Platform.create :name => "Windows", :version => "XP", :regexp => "Windows NT 5.[12]"
     Platform.create :name => "Windows", :version => "Vista", :regexp => "Windows NT 6.0"
     Platform.create :name => "Windows", :version => "7", :regexp => "Windows NT 6.1"
