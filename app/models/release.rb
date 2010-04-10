7692e194c413cab629d841d047febc8f9cc45913
Corban Brook <corbanbrook@gmail.com>
2010-04-10 14:03:14 -0400

    Added test count

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,3 +1,12 @@
 class Release < ActiveRecord::Base
+  has_many :configurations
   default_scope :order => 'version DESC'
+  
+  def self.test_count(release)
+    count = 0
+    release.configurations.each do |c|
+      count += c.tests.count
+    end
+    return count
+  end
 end
